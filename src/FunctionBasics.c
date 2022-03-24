303c12e3a3dfeaee0b8a60f14dd2b75d9bbe487b
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-03-24 11:35:16 +0530

    Add files via upload

---
+++ src/FunctionBasics.c
@@ -0,0 +1,11 @@
+//Function concept in c
+#include<stdio.h>
+void display(){                    //function prototype
+    printf("Good morning Abhishek\n");         //funtion definition
+    printf("be fast u r getting late\n");
+}
+int main(){
+    display();          //funtion call
+    printf("don't forget to take ur lunch box\n");
+    return 0;
+}
