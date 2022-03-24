cab26ad9bc655b31ff225fd6ae17a2e4319f98f6
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-03-24 11:13:51 +0530

    Add files via upload

---
+++ src/ascii_values.c
@@ -0,0 +1,11 @@
+/* Program to print all ASCII Value Table (values and associated character) */
+#include <stdio.h>
+int main()
+{
+    int i;
+    for (i = 0; i <= 255; i++) /* Total 256 ASCII characters are there */
+    {
+        printf("ASCII value of character %c=%d\n", i, i);
+    }
+    return 0;
+}
