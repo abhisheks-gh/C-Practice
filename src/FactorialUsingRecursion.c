7944d9925b7551ac0b558a5336ae2dc06e0f7f33
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-03-24 11:32:48 +0530

    Add files via upload

---
+++ src/FactorialUsingRecursion.c
@@ -0,0 +1,18 @@
+/* Program to find factorial of a number using recursion. */
+#include<stdio.h>
+int factorial(int x);
+int main(){
+    int a;
+    printf("Enter a numeber to find it's factorial\n");
+    scanf("%d",&a);
+    printf("The factorial of %d is %d\n",a,factorial(a));
+    return 0;
+}
+int factorial(int x){
+    if(x==1 || x==0){
+        return 1;
+    }
+    else{
+        return x*factorial(x-1);
+    }
+}
