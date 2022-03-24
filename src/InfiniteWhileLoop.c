a12455d3a9798fbb7f877bd527409f478cc5f2ef
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-03-24 11:37:23 +0530

    Add files via upload

---
+++ src/InfiniteWhileLoop.c
@@ -0,0 +1,17 @@
+/*Program to take input from the user and return numbers next to it(upto 10) if the entered value is less than 10*/
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the value of a\n");
+    scanf("%d", &a);
+    a=11;
+    while (a > 10){  
+        printf("%d\n", a);
+        a++;
+    }
+    return 0;
+}
+
+/*If the condition never becomes false, the while loop keeps getting executed.
+  such a loop is known as infinite loop */
