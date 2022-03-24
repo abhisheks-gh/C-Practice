1bc553132dca55341f54094ddb11cc9f0d3c2be5
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-03-24 11:37:09 +0530

    Add files via upload

---
+++ src/IncomeTax.c
@@ -0,0 +1,24 @@
+/*Program to calculate income tax*/
+#include <stdio.h>
+int main()
+{
+    float tax = 0, income;
+    printf("Enter your income\n"); /*There is no tax below 2.5 lakhs*/
+    scanf("%f",&income);
+    if (income >= 250000 && income <= 500000)
+    {
+        tax = tax + 0.05 * (income - 250000);
+        printf("You have to pay a tax of %f\n", tax);
+    }
+    if (income >= 500000 && income <= 1000000)
+    {
+        tax = tax + 0.20 * (income - 500000);
+        printf("You have to pay a tax of %f\n", tax);
+    }
+    if (income > 1000000)
+    {
+        tax = tax + 0.30 * (income - 1000000);
+        printf("You have to pay a tax of %f\n", tax);
+    }
+    return 0;
+}
