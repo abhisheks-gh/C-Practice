dd9d41ce940e49b2c3b4ac5688a5908d01b0da77
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-03-24 11:33:36 +0530

    Add files via upload

---
+++ src/FirstNnaturalNumbers.c
@@ -0,0 +1,9 @@
+/* Program to print the sum of first 10 natural numbers using while loop. */
+#include<stdio.h>
+int main(){
+    int i=0;
+    while(i<10){
+        printf("The value of i is %d\n",++i);
+    }
+    return 0;
+}
