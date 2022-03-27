4bb81fd6719779b2b9d12dc56f60259bb3f4081a
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-03-27 15:29:28 +0530

    Update file.c

--- src/File Handling/file.c
+++ src/File Handling/file.c
@@ -1,28 +1,31 @@
-//File Handling follows the concept of CURD (CREATE, UPDATE, READ & DELETE) for handling the data.
-//DBMS follows the same but have many more features in addition.
+// File Handling follows the concept of CURD (CREATE, UPDATE, READ & DELETE) for handling the data.
+// DBMS follows the same but have many more features in addition.
+
 #include <stdio.h>
 
 void main()
 {
-    FILE *fp; //Creating a file pointer
+    // Creating a file pointer
+    FILE *fp; 
 
     int roll;
     char name[30];
-
-    fp = fopen("abc.txt", "w+"); // Syntax:- FILE *fopen( const char* filename, const char * mode );
+ 
+    // Syntax:- FILE *fopen( const char* filename, const char * mode );
+    fp = fopen("abc.txt", "w+"); 
 
     if (fp == NULL){
         printf("File doesn't exist"); //While opening(in read mode) if it doesn't already exist.
         return;
     }
 
-    //Buffer:-  A region of a physical memory storage used to temporarily store data while it is being moved from one place to another.
-    //However, a buffer may be used when moving data between processes within a computer.
-    //The buffer is used to provide efficiency for the read operations.
+    // Buffer:-  A region of a physical memory storage used to temporarily store data while it is being moved from one place to another.
+    // However, a buffer may be used when moving data between processes within a computer.
+    // The buffer is used to provide efficiency for the read operations.
 
-    //Syntax:- int fprintf(FILE *stream, const char *format [, argument, ...])  
-    fprintf(fp, "Hello File\nI am writing some data inside you\n\n" ); //Writing into file
-
+    // Syntax:- int fprintf(FILE *stream, const char *format [, argument, ...])  
+    // Writing into file
+    fprintf(fp, "Hello File\nI am writing some data inside you...\n\n" ); 
     printf("Enter Roll : \n");
     scanf("%d", &roll);
     fprintf(fp, "Roll : %d",  roll);
@@ -30,7 +33,8 @@ void main()
     scanf("%s", &name);
     fprintf(fp, "\tName : %s", name);
 
-    //Syntax:- int fscanf(FILE *stream, const char *format [, argument, ...])  
+    // Syntax:- int fscanf(FILE *stream, const char *format [, argument, ...])  
 
-    fclose(fp); //Closing file
+    // Closing file
+    fclose(fp); 
 }
