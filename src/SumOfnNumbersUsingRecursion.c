bad2828af92cd41356aba7234b4a0d529ea5cd5a
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-03-27 14:52:09 +0530

    Add files via upload

---
+++ src/SumOfnNumbersUsingRecursion.c
@@ -0,0 +1,20 @@
+/* Program to find the sum of natural numbers using recursion */
+#include <stdio.h>
+int sum(int n)
+{
+int sum = 0;
+for(int i = 1; i <= n; i++)
+{
+sum += i;
+}
+return sum;
+}
+int main()
+{
+int n;
+printf("\nEnter the number : \n");
+scanf("%d",&n);
+printf("Sum of first %d natural numbers is %d\n",n,sum(n));
+return 0;
+}
+
