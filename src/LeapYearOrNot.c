550adbaffeeefe18d6f2a72d159a62a6a13f7b32
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-03-27 11:50:47 +0530

    Add files via upload

---
+++ src/LeapYearOrNot.c
@@ -0,0 +1,14 @@
+/*Program to find whether a year is leap or leap or not*/
+#include<stdio.h>
+int main(){
+    int year;
+    printf("Enter year\n");
+    scanf("%d",&year);
+    if(year%4==0){
+        printf("This is a leap year\n");
+    }
+    else{
+        printf("This is not a leap year\n");
+    }
+    return 0;
+}
