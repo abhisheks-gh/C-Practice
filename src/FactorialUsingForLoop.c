d420db4f0ef2af55f2c28ba9061a76f03b8893f1
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-03-28 18:56:59 +0530

    Update FactorialUsingForLoop.c

--- src/FactorialUsingForLoop.c
+++ src/FactorialUsingForLoop.c
@@ -1,15 +1,21 @@
-/* Program to find the factorial of a number using for loop. */
+// Program to find the factorial of a number using for loop. 
+
 #include<stdio.h>
-int main(){
+
+int main() 
+{
     int i,n,factorial=1;
     printf("Enter the number\n");
-    scanf("%d",&n);         //Taking the no. from the user
-    for(i=1;i<=n;i++){ 
-        factorial*=i;       // factorial*=i is same as factorial=factorial*i
-    }                     
+    scanf("%d",&n);         
+    for(i=1;i<=n;i++) { 
+        // factorial *= i is same as factorial = factorial*i
+        factorial*=i;       
+    }   
+    
     printf("The factorial of %d is %d\n",n,factorial);
+    
     return 0;
 }
 
-//Factorial of a number n is n*(n-1)*(n-2)*.....*3*2*1
-//Therefore the factorial of a number 5 will be 5*4*3*2*1=120
+// Factorial of a number n is n*(n-1)*(n-2)*.....*3*2*1
+// Therefore the factorial of a number 5 will be 5*4*3*2*1 = 120
