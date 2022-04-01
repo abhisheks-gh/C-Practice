26bb712282a416d45f9c772e0b9a72db3b8436f7
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-04-01 10:58:56 +0530

    Update WhileLoopIntro.c

--- src/WhileLoopIntro.c
+++ src/WhileLoopIntro.c
@@ -1,11 +1,13 @@
-/*Program to take input from the user and return numbers next to it(upto 10) if the entered value is less than 10*/
+// Program to take input from the user and return numbers next to it(upto 10) if the entered value is less than 10
+
 #include <stdio.h>
+
 int main()
 {
     int a;
     printf("Enter the value of a\n");
     scanf("%d", &a);
-    while (a < 10){  
+    while (a < 10) {  
         printf("%d\n", a);
         a++;
     }
