808327f4280b1f88d37b0abcb92d08b8877097b3
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-04-05 16:02:30 +0530

    Update InfiniteWhileLoop.c

--- src/InfiniteWhileLoop.c
+++ src/InfiniteWhileLoop.c
@@ -1,17 +1,19 @@
-/*Program to take input from the user and return numbers next to it(upto 10) if the entered value is less than 10*/
+// Program to take input from the user and return numbers next to it(upto 10) if the entered value is less than 10
+
 #include <stdio.h>
+
 int main()
 {
     int a;
     printf("Enter the value of a\n");
     scanf("%d", &a);
     a=11;
-    while (a > 10){  
+    while (a > 10) {  
         printf("%d\n", a);
         a++;
     }
     return 0;
 }
 
-/*If the condition never becomes false, the while loop keeps getting executed.
-  such a loop is known as infinite loop */
+// If the condition never becomes false, the while loop keeps getting executed.
+// Such a loop is known as infinite loop.
