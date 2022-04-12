10ca6775e0a73587d210d17325c08a2832b406b1
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-04-12 18:13:07 +0530

    Update BreakStatement.c

--- src/BreakStatement.c
+++ src/BreakStatement.c
@@ -1,17 +1,17 @@
 // A program showing the use of break statement
 
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    int i=0;
-    do{
-        printf("The value of i is %d\n",i);
+    int i = 0;
+    do {
+        printf("The value of i is %d\n", i);
         if (i == 0) {
             break;
         }
         i++;
-    }while(i < 10);
+    }while (i < 10);
     
     return 0;    
 }
