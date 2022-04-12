0c4dfbc98cd792743ef67fc271ea0648da16f625
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-04-12 18:21:50 +0530

    Update ContinueStatement.c

--- src/ContinueStatement.c
+++ src/ContinueStatement.c
@@ -1,22 +1,25 @@
-/* A program showing the use of continue statement */
+// Program to show the implementation of continue statement 
+
 #include <stdio.h>
+
 int main()
 {
     int skip = 5;
     int i = 0;
     while (i < 10){
         i++;
-        if(i!=skip){
+        if(i! = skip) {
             continue;
         }
         else{
-            printf("The value of i is %d\n",i);
+            printf("The value of i is %d\n", i);
         }
     }
     return 0;
     
 }
-// IMPORTANT POINTS :-
-/* Sometimes the name of the variable might not indicate the behaviour of the program. */
-/* Break statement completely exits the loop. */
-/* Continue statement skips the particular iteration of the loop. */
+
+// NOTE:
+// Sometimes the name of the variable might not indicate the behaviour of the program.
+// Break statement completely exits the immediate loop.
+// Continue statement skips the particular iteration of the immediate loop.
