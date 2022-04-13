c6cf1577468d2abacedfe90f5eb08f741c730c00
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-04-13 19:40:21 +0530

    Update FactorialUsingWhileLoop.c

--- src/FactorialUsingWhileLoop.c
+++ src/FactorialUsingWhileLoop.c
@@ -1,13 +1,20 @@
-/* To find the factorial of a program using while loop */
-#include<stdio.h>
-int main(){
-    int i=1,n,factorial=1;
+// Program to find the factorial of a number using while loop 
+
+#include <stdio.h>
+
+int main() {
+    int i = 1, n, factorial = 1;
+    
     printf("Enter the number\n");
-    scanf("%d",&n);
-    while(i<=n){
-        factorial*=i;   //factorial*=i is same as factorial=factorial*i
+    scanf("%d", &n);
+    
+    while (i <= n) {
+        // factorial *= i is same as factorial = factorial * i
+        factorial *= i;   
         i++;
     }
-    printf("The factorial of %d is %d\n",n,factorial);
+    
+    printf("The factorial of %d is %d\n", n, factorial);
+    
     return 0; 
 }
