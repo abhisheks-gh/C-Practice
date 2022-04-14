1b1efaf6cb21503d87ef78ddf1b7f0aa196ad943
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-04-14 18:53:58 +0530

    Update SimpleInterest.c

--- src/SimpleInterest.c
+++ src/SimpleInterest.c
@@ -1,16 +1,22 @@
-/*Program to calculate simple interest*/
+// Program to calculate simple interest
+
 #include <stdio.h>
+
 int main()
 {
     float p, t, r, si;
+    
     printf("Enter Principle amount\n");
-    scanf("%f",&p);
-     printf("Enter time for which loan is taken\n");
-    scanf("%f",&p);
-     printf("Enter rate of interest\n");
-    scanf("%f",&p);
-    //formula for simple interest//
-    si = (p * r * t )/ 100;
+    scanf("%f", &p);
+    printf("Enter time for which loan is taken\n");
+    scanf("%f", &t);
+    printf("Enter rate of interest\n");
+    scanf("%f", &r);
+    
+    // Formulae for Simple Interest
+    si = (p * r * t ) / 100;
+    
     printf("%f/n", si);
+    
     return 0;
 }
