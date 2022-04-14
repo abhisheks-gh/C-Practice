eaba83c573b421d52dbc91b0caf81cd998c37e9e
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-04-14 18:51:45 +0530

    Update SmallestElementInArray.c

--- src/SmallestElementInArray.c
+++ src/SmallestElementInArray.c
@@ -1,24 +1,25 @@
-/* Program to find smallest element in array */
+// Program to find the smallest element in an array
+
 #include <stdio.h>
+
 int main()
 {
     int i, j, k;
-    int num[3]; /*Array declaration*/
+    int num[3]; 
+    
     printf("Enter three numbers\n");
-    scanf("%d", &i); /*Taking three numbers from the user*/
+    scanf("%d", &i); 
     scanf("%d", &j);
     scanf("%d", &k);
+    
     if (i < j && i < k)
-    {
         printf("%d is the smallest element in this array\n", i);
-    }
+    
     if (j < i && j < k)
-    {
         printf("%d is the smallest element in this array\n", j);
-    }
+    
     if (k < i && k < j)
-    {
         printf("%d is the smallest element in this array\n", k);
-    }
+        
     return 0;
 }
