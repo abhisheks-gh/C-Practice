b343b6b1077e6b4f498864a5e6ae99bd5d45f01f
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-04-14 18:42:00 +0530

    Update SumOfnNumbersUsingRecursion.c

--- src/SumOfnNumbersUsingRecursion.c
+++ src/SumOfnNumbersUsingRecursion.c
@@ -1,20 +1,23 @@
-/* Program to find the sum of natural numbers using recursion */
+// Program to find the sum of natural numbers using recursion 
+
 #include <stdio.h>
+
 int sum(int n)
 {
-int sum = 0;
-for(int i = 1; i <= n; i++)
-{
-sum += i;
-}
-return sum;
-}
-int main()
-{
-int n;
-printf("\nEnter the number : \n");
-scanf("%d",&n);
-printf("Sum of first %d natural numbers is %d\n",n,sum(n));
-return 0;
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    sum += i;
+  
+    return sum;
 }
 
+int main()
+{
+    int n;
+    printf("\nEnter the number : \n");
+    scanf("%d", &n);
+  
+    printf("Sum of first %d natural numbers is %d\n", n, sum(n));
+  
+    return 0;
+}
