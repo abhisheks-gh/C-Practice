3b366fa209a26943079eaf80f153933c2c1bad57
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-04-16 14:57:58 +0530

    Update MultiplicationTable.c

--- src/MultiplicationTable.c
+++ src/MultiplicationTable.c
@@ -1,11 +1,16 @@
-/* Program to print the multiplication of a number n in reverse order. */
-#include<stdio.h>
-int main(){
+// Program to print the multiplication of a number n in reverse order.
+
+#include <stdio.h>
+
+int main() {
     int i,n;
+    
     printf("Enter the number\n");
-    scanf("%d",&n);
-    for(i=10;i>0;i--){
-        printf("The value of n is %d\n",i*n);
+    scanf("%d", &n);
+    
+    for (i = 10; i > 0; i--) {
+        printf("The value of n is %d\n", i * n);
     }
+    
     return 0;
 }
