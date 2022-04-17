76541dd10aab7476549df95417f9e3a9680a12c1
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-04-17 14:43:02 +0530

    Update SetOperations.c

--- src/SetOperations.c
+++ src/SetOperations.c
@@ -1,4 +1,4 @@
-// Program to implement set operations.
+// Program to implement set operations
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,76 +6,73 @@
 void intersection_set(int arr1[11],int arr2[11])
 {
     printf("Intersection of A and B = ");
-    for (int i=0;i<11;i++)
+    for (int i = 0; i < 11; i++)
         {
-            int k=0;
-            for (int j=0;j<11;j++)
+            int k = 0;
+            for (int j = 0; j < 11; j++)
             {
-                if (arr1[i]==arr2[j])
+                if (arr1[i] == arr2[j])
                 {
-                    k=1;
+                    k = 1;
                     break;
                 }
             }
-            if (k==1)
-            printf("%d  ",arr1[i]);
+            if (k == 1)
+            printf("%d  ", arr1[i]);
         }
 }
 
-void set_difference(int arr1[11],int arr2[11])
+void set_difference(int arr1[11], int arr2[11])
 {
     printf("Set Difference A-B = \n");
-    for (int i=0;i<11;i++)
+    for (int i = 0; i < 11; i++)
     {
-        int k=0;
-        for (int j=0;j<11;j++)
+        int k = 0;
+        for (int j = 0; j < 11; j++)
         {
-            if (arr1[i]==arr2[j])
+            if (arr1[i] == arr2[j])
             {
-                k=1;
+                k = 1;
                 break;
             }
         }
-        if (k==0)
-        printf("%d  ",arr1[i]);
+        if (k == 0)
+        printf("%d  ", arr1[i]);
     }
 }
 
-void union_set(int arr1[11],int arr2[11],int arr3[22])
+void union_set(int arr1[11], int arr2[11], int arr3[22])
 {
     int i;
-        for (i=0;i<11;i++)
-        {
-            arr3[i]=arr1[i];
-        }
-        for (i=0;i<11;i++)
-        {
-            arr3[11+i]=arr2[i];
-        }
-        printf("The Union Of 2 Sets is = ");
-        for (i=0;i<22;i++)
-        {
-            int k=0;
-            for (int j=i+1;j<22;j++)
-            {
-                if (arr3[i]==arr3[j])
-                {
-                    k=1;
-                    break;
-                }
-            }
-            if (k==0)
-                {
-                    printf("%d  ",arr3[i]);
-                }
-        }
+    
+    for (i = 0; i < 11; i++)
+            arr3[i] = arr1[i];
+    
+    for (i = 0; i < 11; i++)
+            arr3[11+i] = arr2[i];
         
+    printf("The Union Of 2 Sets is = ");
+    for (i = 0; i < 22; i++)
+    {
+        int k = 0;
+        for (int j = i+1; j < 22; j++)
+        {
+            if (arr3[i] == arr3[j])
+            {
+                k = 1;
+                break;
+            }
+        }
+            if (k == 0)
+                printf("%d  ",arr3[i]);
+        }
 }
+
 int main ()
 {
-    int c,i;
-    int arr1[11]={1,3,5,7,9};
-    int arr2[11]={9,4,6,8,10};
+    int c, i;
+    int arr1[11] = {1, 3, 5, 7, 9};
+    int arr2[11] = {9, 4, 6, 8, 10};
     int arr3[22];
     char ch;
     system("CLS");
@@ -84,9 +81,9 @@ int main ()
     printf("2       Intersection Set\n");
     printf("3       Set Difference\n");
     printf("Choice  = ");
-    scanf("%d",&c);
-    switch (c)
-        {
+    scanf("%d", &c);
+    switch(c)
+    {
         case 1:
         union_set(arr1,arr2,arr3);
         break;
@@ -102,6 +99,6 @@ int main ()
         default :
         printf("!!ERROR !!\nWrong Choice");
         break;
-        }
+    }
     return 0;
 }
