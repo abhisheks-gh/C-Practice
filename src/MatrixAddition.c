a520de294295c977b5871d8ef127d31a48cb3798
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-04-17 15:08:13 +0530

    Update MatrixAddition.c

--- src/MatrixAddition.c
+++ src/MatrixAddition.c
@@ -1,7 +1,10 @@
-//Addition of two matrices//
+// Addition of two matrices
+
 #include <stdio.h>
 #include<conio.h>
-int main() {
+
+int main() 
+{
   int r, c, a[100][100], b[100][100], sum[100][100], i, j;
   printf("Enter the number of rows: ");
   scanf("%d", &r);
