03bee240aec2157183216f2142433bfaefb1487c
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-04-17 14:46:29 +0530

    Update PrimeCheck.c

--- src/PrimeCheck.c
+++ src/PrimeCheck.c
@@ -1,21 +1,23 @@
-/* Program to check whether a number is prime or not. */
+// Program to check whether a number is prime or not
+
 #include<stdio.h>
-int main(){    
-int n,i,m=0,flag=0;    
-printf("Enter the number to be checked :\n");    
-scanf("%d",&n);    
-m=n/2;    
-for(i=2;i<=m;i++)    
+
+int main() 
 {    
-if(n%i==0)    
-{    
-printf("%d is not prime\n",n);    
-flag=1;    
-break;    
-}    
-}    
-if(flag==0)    
-printf("%d is prime\n",n);     
-return 0;  
- }   
-  
+    int n, i, m = 0, flag = 0;    
+    printf("Enter the number to be checked :\n");    
+    scanf("%d", &n);    
+    m = n/2;    
+    for (i = 2; i <= m; i++)    
+    {    
+     if(n % i == 0)    
+     {    
+     printf("%d is not prime\n", n);    
+     flag = 1;    
+     break;    
+     }    
+    }    
+    if(flag == 0)    
+    printf("%d is prime\n", n);     
+    return 0; 
+}   
