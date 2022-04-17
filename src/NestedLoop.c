e2bc83c62f26ebc3b2a928e6656e1b5854cab53f
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-04-17 15:01:26 +0530

    Update NestedLoop.c

--- src/NestedLoop.c
+++ src/NestedLoop.c
@@ -1,20 +1,23 @@
-/*Program to print natural numbers between 10 to 20 when the initial loop count is initialized to 0*/
+// Program to print natural numbers between 10 to 20 when the initial loop count is initialized to 0
+
 #include<stdio.h>
-int main(){
+
+int main()
+{
     int i;
-    i=0;
-    while(i<=20){
-        if(i>=10){
-            printf("The value of i is %d\n",i);
+    i = 0;
+    while ( i <= 20) {
+        if (i >= 10) {
+            printf("The value of i is %d\n", i);
         }
         i++;
     }
     return 0;
 }
 
-/* Increment/decrement operators :-
-i++  --> First print then increment
-++i  --> First increment then print
-i--  -->First print then decrement
---i  -->First decrement then print */
-  
+
+// Increment/decrement operators:
+    // i++  --> First print then increment
+    // ++i  --> First increment then print
+    // i--  -->First print then decrement
+    // --i  -->First decrement then print
