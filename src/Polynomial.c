b9f7b0efdf57ee8dd22454af98bb7e695ab09bc0
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-04-17 14:50:05 +0530

    Update Polynomial.c

--- src/Polynomial.c
+++ src/Polynomial.c
@@ -1,28 +1,31 @@
-// Program to calculate value of polynomial for variable x.
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+// Program to calculate value of polynomial for variable x
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
 #pragma warn-rvl
+
 void main()
 { 
-  int i,j,x,value=0,m,power,a[10];
+  int i, j, x, value = 0, m, power, a[10];
   system("CLS");
   printf("\nEnter the highest power of x :");
-  scanf("%d",&i);
+  scanf("%d", &i);
   
   printf("\nEnter the coefficients of powers of x in the ascending order: ");
-  for(j=0;j<=i;j++)
+  for (j = 0; j <= i; j++)
   {
-    printf("\ncoeff of x to power %d:",j);
-    scanf("%d",&a[j]);
+    printf("\ncoeff of x to power %d:", j);
+    scanf("%d", &a[j]);
   }
   printf("\nEnter the value of x: ");
-  scanf("%d",&x);
+  scanf("%d", &x);
 
-for(m=0;m<=i;m++)
-{  power=pow(x,m);
-   value=value+(a[m]*power); 
-}
-printf("The the value of the entered polynomial for x = %d is : %d",x,value);
-
+  for (m = 0; m <= i; m++)
+  {  
+  power = pow(x, m);
+  value = value + (a[m] * power); 
+  }
+  printf("The the value of the entered polynomial for x = %d is : %d", x, value);
 }
