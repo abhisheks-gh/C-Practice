54a7c0f0f70a2d20e6d7b020f9b74ded99c401cc
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-04-23 17:53:18 +0530

    Update FunctionBasics.c

--- src/FunctionBasics.c
+++ src/FunctionBasics.c
@@ -1,11 +1,13 @@
-//Function concept in c
+// Functions concept
+
 #include<stdio.h>
-void display(){                    //function prototype
-    printf("Good morning Abhishek\n");         //funtion definition
+
+void display() {                               // function prototype
+    printf("Good morning Abhishek\n");         // funtion definition
     printf("be fast u r getting late\n");
 }
 int main(){
-    display();          //funtion call
+    display();          // funtion call
     printf("don't forget to take ur lunch box\n");
     return 0;
 }
