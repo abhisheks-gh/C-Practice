04067796ece7e39ed56381c4743e0c0ef9132a3b
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-04-23 18:01:11 +0530

    Update InvertedPyramid.c

--- src/InvertedPyramid.c
+++ src/InvertedPyramid.c
@@ -1,18 +1,21 @@
+// Program to print a inverted pyramid
+
 #include <stdio.h>  
   
 int main()  
 {  
-    int n,m=1;  
-    printf("Enter the number of rows ");  
-    scanf("%d",&n);  
+    int n, m = 1;
   
-   for(int i=n;i>=1;i--)  
+    printf("Enter the number of rows ");  
+    scanf("%d", &n);  
+  
+   for (int i = n; i >= 1; i--)  
    {  
-       for(int j=1;j<m;j++)  
+       for (int j = 1; j < m; j++)  
        {  
            printf(" ");  
        }  
-       for(int k=1;k<=2*i-1;k++)  
+       for (int k = 1;k <= 2*i-1; k++)  
        {  
            printf("*");  
        }  
