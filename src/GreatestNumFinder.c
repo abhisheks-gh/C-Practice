12d7284e44221f674c7e91afc385f2bd7775e4c6
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-04-23 17:58:01 +0530

    Update GreatestNumFinder.c

--- src/GreatestNumFinder.c
+++ src/GreatestNumFinder.c
@@ -1,26 +1,27 @@
-/*Program to find greatest of four numbers entered by the user*/
-#include<stdio.h>
-int main(){
-    int num1,num2,num3,num4;
+// Program to find greatest of four numbers entered by the user
+
+#include <stdio.h>
+
+int main() {
+    int num1, num2, num3, num4;
+    
     printf("Enter the first number\n");
-    scanf("%d",&num1);
+    scanf("%d", &num1);
      printf("Enter the second number\n");
-    scanf("%d",&num2);
+    scanf("%d", &num2);
     printf("Enter the third number\n");
-    scanf("%d",&num3);
+    scanf("%d", &num3);
     printf("Enter the fourth number\n");
-    scanf("%d",&num4);
-    if(num1>num2 && num1>num3 && num1>num4){
+    scanf("%d", &num4);
+    
+    if (num1 > num2 && num1 > num3 && num1 > num4)
         printf("%d is greatest among the above numbers\n",num1);
-    }
-     if(num2>num1 && num2>num3 && num2>num4){
+     if(num2 > num1 && num2 > num3 && num2 > num4)
         printf("%d is greatest among the above numbers\n",num2);
-    }
-     if(num3>num2 && num3>num1 && num1>num4){
+     if(num3 > num2 && num3 > num1 && num1 > num4)
         printf("%d is greatest among the above numbers\n",num3);
-    }
-     if(num4>num2 && num4>num3 && num4>num1){
+     if(num4 > num2 && num4 > num3 && num4 > num1)
         printf("%d is greatest among the above numbers\n",num4);
-    }
+    
     return 0;   
 }
