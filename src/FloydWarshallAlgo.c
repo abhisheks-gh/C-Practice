982fe92d49a4ac8a7a6bbf7da66cd46dafc1b391
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-04-24 00:30:33 +0530

    Update FloydWarshallAlgo.c

--- src/FloydWarshallAlgo.c
+++ src/FloydWarshallAlgo.c
@@ -27,44 +27,46 @@ void main()
         if (i == j)
           B[i][j] = 0;
         else {
-          printf("\nform: %s to %s",A[i],A[j]);
-         scanf("%d",&B[i][j]);
-         if(B[i][j]==0)
-         B[i][j]=9000;
+          printf("\nform: %s to %s", A[i], A[j]);
+         scanf("%d", &B[i][j]);
+         if (B[i][j] == 0)
+          B[i][j]=9000;
         }
      }
   }
  printf("\n --------------------ORIGINAL INPUTS in form of matrix path is row to column:------------------------\n");
- for(i=0;i<n;i++)
+ for (i = 0; i < n; i++)
  {  
-    printf(" %s ",A[i]);
+    printf(" %s ", A[i]);
  }
- for(i=0;i<n;i++)
- { printf("\n%s",A[i]);
-   for(j=0;j<n;j++)
+ for (i = 0; i < n; i++)
+ { 
+   printf("\n%s", A[i]);
+   for (j = 0; j < n; j++)
    { 
-     printf(" %d ",B[i][j]);
+     printf(" %d ", B[i][j]);
    }
  }
 
- for(k=0;k<n;k++)
-   {   for(i=0;i<n;i++)
-         {  for(j=0;j<n;j++)
-              { if(B[i][j]>B[i][k]+B[k][j])
-                B[i][j]=B[i][k]+B[k][j]; 
+ for (k = 0; k < n; k++)
+   {   for (i = 0; i < n; i++)
+         {  for (j = 0; j < n; j++)
+              {
+                if (B[i][j] > B[i][k] + B[k][j])
+                    B[i][j] = B[i][k] + B[k][j]; 
               }
          }
    }
    printf("\n  ------------------Using FLOYD WARHALL'S ALGORITHM we get the shoretes path in the form of matrix:------------- \n");
-   for(i=0;i<n;i++)
+   for (i = 0; i < n; i++)
  {  
-    printf(" %s ",A[i]);
+    printf(" %s ", A[i]);
  }
-   for(i=0;i<n;i++)
-    { printf("\n%s",A[i]);
-       for(j=0;j<n;j++)
+   for (i = 0; i < n; i++)
+    { printf("\n%s", A[i]);
+       for (j = 0; j < n; j++)
        { 
-         printf(" %d ",B[i][j]);
+         printf(" %d ", B[i][j]);
        }
     }
 }
