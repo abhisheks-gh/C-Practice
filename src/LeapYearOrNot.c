b3a5ff9eddb0ae7cf6b69fb2aa2e7377944db2af
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-04-24 00:32:35 +0530

    Update LeapYearOrNot.c

--- src/LeapYearOrNot.c
+++ src/LeapYearOrNot.c
@@ -2,17 +2,16 @@
 
 #include <stdio.h> 
 
-int main() {
+int main() 
+{
     int year;
     printf("Enter year\n");
     scanf("%d",&year);
     
-    if(year % 4 == 0) {
+    if(year % 4 == 0)
         printf("This is a leap year\n");
-    }
-    else {
+    else
         printf("This is not a leap year\n");
-    }
     
     return 0;
 }
