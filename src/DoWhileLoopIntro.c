d196458fd629a2c13cc21905e144d433358bc989
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-04-23 17:51:34 +0530

    Update DoWhileLoopIntro.c

--- src/DoWhileLoopIntro.c
+++ src/DoWhileLoopIntro.c
@@ -1,17 +1,20 @@
 // Program to print first n natural numbers
-/* Input = 4 and
-   Output = 1
-            2
-            3
-            4 */
+
+//    Input = 4 and
+//    Output = 1
+//             2
+//             3
+//             4 
+
+
 #include <stdio.h>
+
 int main()
 {
    int i = 0, n;
    printf("Enter the value of n\n");
    scanf("%d", &n);
-   do
-   {
+   do {
       printf("The value of n is %d\n", i+1);
       i++;
    } while (i < n);
